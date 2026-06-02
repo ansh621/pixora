dce1ddf0bf9874107df4ee9d7aae2c0320bb15c8
Ansh <vermaansh53@gmail.com>
2026-06-03 01:56:09 +0530

    Fix CloudinaryStorage import - handle default and regular exports

--- routes/multer.js
+++ routes/multer.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
-const CloudinaryStorage = require('multer-storage-cloudinary');
+const CloudinaryStorageModule = require('multer-storage-cloudinary');
+const CloudinaryStorage = CloudinaryStorageModule.default || CloudinaryStorageModule;
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
